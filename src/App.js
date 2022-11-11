c82c7c965cddd2b80c7bea1ace6c5061e39e502a
KlaudiaKutypa <klaudia.kutypa@gmail.com>
2022-11-11 20:43:09 +0100

    update

--- src/App.js
+++ src/App.js
@@ -44,28 +44,28 @@ function App() {
           <TextField id="outlined-basic" label="ID number only..." variant="outlined" type="number" onChange={(e) => setNumber(e.target.value)}></TextField>
         </Box>
         <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" sx={{ fontWeight: 700, fontSize: "1.1rem" }}>ID</TableCell>
-              <TableCell align="center" sx={{ fontWeight: 700, fontSize: "1.1rem" }}>Name</TableCell>
-              <TableCell align="center" sx={{ fontWeight: 700, fontSize: "1.1rem" }}>Year</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-         {all.filter(ev => {
-            if (number === "") {
-            return ev}
-            else if (ev.id == number) {
-            return ev}}).slice(indexOfFirst, indexOfLast).map((prod) => (
-            <TableRow key={prod.id}>
-            <TableCell align="center" sx = {{backgroundColor: prod.color, fontSize: "1.1rem"}}>{prod.id}</TableCell>
-            <TableCell align="center" sx = {{backgroundColor: prod.color, fontSize: "1.1rem"}}>{prod.name}</TableCell>
-            <TableCell align="center" sx = {{backgroundColor: prod.color, fontSize: "1.1rem"}}>{prod.year}</TableCell>
-            </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center" sx={{ fontWeight: 700, fontSize: "1.1rem" }}>ID</TableCell>
+                <TableCell align="center" sx={{ fontWeight: 700, fontSize: "1.1rem" }}>Name</TableCell>
+                <TableCell align="center" sx={{ fontWeight: 700, fontSize: "1.1rem" }}>Year</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {all.filter(ev => {
+              if (number === "") {
+              return ev}
+              else if (ev.id == number) {
+              return ev}}).slice(indexOfFirst, indexOfLast).map((prod) => (
+                <TableRow key={prod.id}>
+                  <TableCell align="center" sx = {{backgroundColor: prod.color, fontSize: "1.1rem"}}>{prod.id}</TableCell>
+                  <TableCell align="center" sx = {{backgroundColor: prod.color, fontSize: "1.1rem"}}>{prod.name}</TableCell>
+                  <TableCell align="center" sx = {{backgroundColor: prod.color, fontSize: "1.1rem"}}>{prod.year}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </TableContainer>
         <AppPagination currentPage={currentPage} handleChange={handleChange} />
       </div>
